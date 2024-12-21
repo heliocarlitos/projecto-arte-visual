beadb5f845740a804ef7990d4163d3482735deee
Hélio Carlitos <112761333+heliocarlitos@users.noreply.github.com>
2024-12-21 08:03:12 -0300

    Create HeroSection.jsx

---
+++ src/Componentes/Layout/HeroSection.jsx
@@ -0,0 +1,71 @@
+import { SiAdobephotoshop, SiWondersharefilmora } from "react-icons/si";
+import { IoLogoFigma } from "react-icons/io5";
+import { RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
+import { FaReact } from "react-icons/fa";
+
+function HeroSection() {
+    return (
+        <>
+            <section className="HeroSection">
+
+                <div className="Icones">
+
+                    <div className="Icon1">
+                        <figure><img src="https://crypto-tailwind.preview.uideck.com/src/images/shapes/hero-shape-1.svg" alt="svg" /></figure>
+                    </div>
+
+                    <div className="Icon2">
+                        <figure><img src="https://crypto-tailwind.preview.uideck.com/src/images/shapes/hero-shape-2.svg" alt="svg" /></figure>
+                    </div>
+
+                </div>
+
+                <div className="Conteudo">
+
+                    <div className="Titulo">
+                        <p>Sua agência de design gráfico e criação de conteúdos digitais</p>
+                    </div>
+
+                    <div className="Texto">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas odio pariatur excepturi mollitia, perferendis quisquam, ab, ad ullam nihil consequatur doloremque facere magnam! Facilis ex natus fuga ut explicabo veniam?</p>
+                    </div>
+
+                    <div className="Ferramentas">
+                        <div className="Icon">
+                            <SiAdobephotoshop />
+                        </div>
+
+                        <div className="Icon">
+                            <IoLogoFigma />
+                        </div>
+
+                        <div className="Icon">
+                            <SiWondersharefilmora />
+                        </div>
+
+                        <div className="Icon">
+                            <FaReact />
+                        </div>
+
+                        <div className="Icon">
+                            <RiNextjsFill />
+                        </div>
+
+                        <div className="Icon">
+                            <RiTailwindCssFill />
+                        </div>
+
+                    </div>
+
+                    <div className="Btn">
+                        <a href="#" aria-label="Começar">Começar</a>
+                    </div>
+
+                </div>
+
+            </section>
+
+        </>
+    )
+}
+export default HeroSection;
